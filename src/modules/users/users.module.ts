9e23ea828282b22ceef29be70a040529d485490b
Mohamed Atef <atefm.me499@gmail.com>
2025-08-20 11:51:34 +0300

    changes

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
+import { PermissionModule } from '../../shared/permission/permission.module';
 
 @Module({
+  imports: [PermissionModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersService, UsersRepository],
