a77f2d4da831ae60b0986867a090eb64d4dc9a42
Mohamed Atef <atefm.me499@gmail.com>
2025-08-31 13:10:31 +0300

    changes

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -6,29 +6,30 @@ import {
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from '../decorators/roles.decorator';
-import { Role } from '@prisma/client';
+import { RoleName } from '@prisma/client';
+import { AuthUser } from '../../modules/auth/interfaces/auth-user.interface';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
+    const requiredRoles = this.reflector.getAllAndOverride<RoleName[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
 
     if (!requiredRoles || requiredRoles.length === 0) return true;
 
     const req = context.switchToHttp().getRequest();
-    const user = req.user;
+    const user: AuthUser | undefined = req.user;
 
-    if (!user?.role) {
+    if (!user?.role?.name) {
       throw new ForbiddenException('User role not found');
     }
 
-    if (!requiredRoles.includes(user.role)) {
-      throw new ForbiddenException(`Access denied for role ${user.role}`);
+    if (!requiredRoles.includes(user.role.name)) {
+      throw new ForbiddenException(`Access denied for role ${user.role.name}`);
     }
 
     return true;
